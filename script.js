a7709197804214ee664c1ab35fea4cd3c32deb53
Arriero82 <gabriel.vullioud@gmail.com>
2022-05-11 16:53:25 +0000

    finish 2nd exercise of day 2

--- script.js
+++ script.js
@@ -94,4 +94,18 @@ function lonelyinteger(a) {
 }
 
 
-console.log(lonelyinteger([1,2,3,4,4,3,2,1,8]));
+//console.log(lonelyinteger([1,2,3,4,4,3,2,1,8]));
+
+function diagonalDifference(arr) {
+    let leftToRight = 0;
+    let rightToLeft = 0;
+    for(let i=0;i<arr.length;i++){
+        leftToRight += arr[i][i];
+        rightToLeft += arr[i][arr.length -1 - i];
+    }
+
+    return Math.abs(leftToRight-rightToLeft);
+}
+
+const arr = [[11,2,4],[4,5,6],[10,8,-12]];
+console.log(diagonalDifference(arr));
